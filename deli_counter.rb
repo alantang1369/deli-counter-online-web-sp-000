2a32b48a4c6363999cb84d4a16e8dc3f19e41458
Alan Tang <giabao_tang@yahoo.com>
2020-09-30 23:11:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,7 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    status = 
+    status = "The line is currently:"
+    array.each_with_index do |name, index|
+      status + " #{index + 1}. {#{name}"
     
